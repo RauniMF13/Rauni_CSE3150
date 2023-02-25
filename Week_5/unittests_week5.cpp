b8b942322ae848fa1d9e263b6fc20e9bcf7c2a5a
Rauni_Machado-Filho <rauni.machado-filho@uconn.edu>
2023-02-25 13:26:06 -0500

    Week_5 Unit Tests

--- Week_5/unittests_week5.cpp
+++ Week_5/unittests_week5.cpp
@@ -1,20 +1,28 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
 #include "students.h"
+#include "../../doctest/doctest/doctest.h"
+
 #include <iostream>
 
 using namespace std;
 
-int main() {
-  struct student* my_student = create_student(243716, "John Doe", 80, 92);
-
-  my_student->hw_grades.push_back(75);
+TEST_CASE("Students structs and methods functionality") {
+  struct student* my_student = create_student(243716, "John Smith", 80, 92);
   
+  my_student->hw_grades.push_back(75);
+
+  CHECK(my_student->grade() == 82.1);
   print_student(my_student);
   
   int length_student_list = 10;
   struct student** students_list = generate_students(length_student_list, &random_fcn);
 
-  for (int i = 0; i < length_student_list; i++) {
-    print_student(students_list[i]);
+  for(int i = 0; i < length_student_list; i++) {
+    CHECK(students_list[i]->id == (i + 100000));
+    CHECK(students_list[i]->final > 0);
+    CHECK(students_list[i]->midterm > 0);
+    CHECK(students_list[i]->grade() > 0);
   }
   
   delete my_student;
