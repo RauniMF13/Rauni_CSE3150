2163802c87a840af9824bc1c1fc7e4b65218a149
Rauni_Machado-Filho <rauni.machado-filho@uconn.edu>
2023-02-25 13:40:44 -0500

    Week_5 Unit Tests

--- Week_5/unittests_week5.cpp
+++ Week_5/unittests_week5.cpp
@@ -24,7 +24,18 @@ TEST_CASE("Students structs and methods functionality") {
     CHECK(students_list[i]->midterm > 0);
     CHECK(students_list[i]->grade() > 0);
   }
+
+  int length_definite_list = 5;
+  struct student** students_definite = generate_students(length_definite_list, &non_random_fcn);
+
+  for(int i = 0; i < length_definite_list; i++) {
+    CHECK(students_definite[i]->id == (i + 100000));
+    CHECK(students_definite[i]->final == 75);
+    CHECK(students_definite[i]->midterm == 75);
+    CHECK(students_definite[i]->grade() == 75);
+  }
   
   delete my_student;
   free_student_data(students_list, length_student_list);
+  free_student_data(students_definite, length_definite_list);
 }
